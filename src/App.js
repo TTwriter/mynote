a748a5beb712c3149e8cd99892c4ef6bd10448c0
TTwriter <342507836@qq.com>
2020-04-12 01:15:18 +0800

    '重搭建项目'

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import 'antd/dist/antd.css'
-import Button from 'antd/es/button'
-import React from 'react'
-import './App.css'
-import {Col,Row} from 'antd'
-import Header from './components/header/header'
-import Index from './components/index/index'
-import Add from './components/example/add'
-import List from './components/example/list'
-import Login from './components/login'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// function App() { // {/*简单组件方法 function 方法名*/}
-
-class App extends React.Component{  // {/* 复杂组件方法*/}
-  state = {
-    list:[]
-  } 
-  componentWillMount(){
-    console.log("组件挂载之前")
-    
-  }
-  componentDidMount() { //
-    setTimeout(() => {
-      this.setState({ //setState方法 可以更新数据
-        list:['传值','传值','方式']
-      })
-    }, 1000)
-  }
-  add = (value)=>{
-    console.log(value)
-    const {list} = this.state
-    list.unshift(value)
-    this.setState({
-      list
-    })
-  }
-  render(){ 
-    const {list} = this.state
-    const length = list.length
-    return (
-      <Row className='container'>
-        <Header/>
-        <Col className="main">
-              <Index />  
-              <Add allAdd={this.add} length={length}/>
-              
-              <List list={list} />
-              <Button type="primary">Button</Button>
-          </Col>
-      </Row>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/layout/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Layout, Menu, Icon, Form } from 'antd'
-// import ReturnFactory from ''
-const {Header, Slide, Content } = Layout
-// require("../")
